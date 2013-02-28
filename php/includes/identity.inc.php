02994c9aa6c4713c91c0edef7d18d00e4718d7bd
maxime.eloy <maxime.eloy@free.fr>
2013-02-28 19:23:53 +0100

    formulaire connexion

--- php/includes/identity.inc.php
+++ php/includes/identity.inc.php
@@ -1,15 +1,58 @@
 <?php
 session_start();
-	 $_SESSION['Acces_ad'] = 0;
-	 include 'public/connectTobase.php';
-?>
+	$_SESSION['Acces_ad'] = 0;
+	include ('php/public/connectTobase.php');
+
+	 
+//début du code trouvé
+
+if(isset($_POST) && !empty($_POST['login']) && !empty($_POST['pass'])) {
+  extract($_POST);
+  // on recupère le password de la table qui correspond au login du visiteur
+  $sql = "SELECT pass 
+			FROM adherents 
+			WHERE mail='".$login."'";
+/*  $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());	*/
+ 
+  /*la mienne*/
+  $req = $bdd->query($sql);
+  $data= $req->fetch();
+
+ /* $data = mysql_fetch_assoc($req);*/
+
+	echo $data['pass'];
+	echo $pass;
+	echo 'toto';
+	
+  if($data['pass'] != sha1($pass)) {
+   
+     include('html/login.html'); // On inclut le formulaire d'identification
+	  echo '<p>Mauvais login / password. Merci de recommencer</p>';
+    exit;
+  }
+  else {
+    session_start();
+    $_SESSION['login'] = $login;
+    
+    echo 'Vous etes bien logué';
+    // ici vous pouvez afficher un lien pour renvoyer
+	header("location: index2.php?page=communaute");
+    // vers la page d'accueil de votre espace membres 
+  }    
+}
+else {
+//  echo '<p>Vous avez oublié de remplir un champ.</p>';
+   include('html/login.html'); // On inclut le formulaire d'identification
+   exit;
+}
 
 
-<h1>Connexion</h1>
 
-<?php
+//fin du code trouvé	 
+	 
 
 // variables du formulaire
+/*
 $action = isset($_POST['action']) ? $_POST['action'] : '';  
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
@@ -18,7 +61,7 @@ $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 // Si aucune action, le formulaire est affiché
 if ($action !=1 ) {
 
-echo 'Veuillez vous identifier:<br />';
+echo '<h1>Connexion</h1><br />';
 echo '<form action="'.$_SERVER['REQUEST_URI'].'" method="post">';
 echo '<input type="hidden" name="action" value="1">';
 echo 'Email: <input type="text" name="email"><br />';
@@ -26,7 +69,7 @@ echo 'Password: <input type="password" name="pass"><br />';
 echo '<input type="submit" value="Connexion">';
 echo '</form>';
 
-echo '<a href="inscriptionTobase.php"> Devenir Membres </a></p>';
+echo '<a href="inscriptionTobase.php"> Devenir membre </a></p>';
 echo '<a href="../../index2.php?page=presentation"> Visiteur </a></p>';
 
 
@@ -37,11 +80,9 @@ echo '<a href="../../index2.php?page=presentation"> Visiteur </a></p>';
 // Sinon
 if ($action == 1) {
 
-echo $pass;
 
 $pass=sha1($pass);
 
-echo $pass;
 
 $q = $bdd->query("SELECT COUNT(*) 
                   FROM adherents 
@@ -66,9 +107,9 @@ $n=$q->fetch();
 
          else{
 
-echo 'Veuillez entrer votre Email et Mot de Passe:<br />';
+echo '<h1>Connexion</h1><br />';
 echo '<form action="'.$_SERVER['REQUEST_URI'].'" method="post">';
-echo '<input type="hidden" name="action" value="1">';
+echo '<input type="hidden" name="action" value="0">';
 echo 'Email: <input type="text" name="email"><br />';
 echo 'Passe: <input type="password" name="pass"><br />';
 echo '<input type="submit" value="Connexion">';
@@ -77,6 +118,7 @@ echo '</form>';
 die('<font color="red">Informations incorrectes!');
            }
          }
+*/
 ?>
 
 
